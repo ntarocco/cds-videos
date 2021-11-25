5faec56cc9b0cdaf4c019f608468abbf1233bcf9
Nicola Tarocco <ntarocco@gmail.com>
2021-11-25 11:46:00 +0100

    wip

--- cds/modules/files/ext.py
+++ cds/modules/files/ext.py
@@ -44,8 +44,6 @@ class CDSFilesRestApp(object):
         app.extensions['cds-files-rest'] = self
         self.register_signals(app)
 
-    @staticmethod
-    def register_signals(app):
+    def register_signals(self, app):
         """Register CDS files rest signals."""
-        file_downloaded.connect(
-            on_download_rename_file, sender=app, weak=False)
+        file_downloaded.connect(on_download_rename_file, weak=False)
